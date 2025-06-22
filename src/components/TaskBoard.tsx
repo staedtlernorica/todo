2cc490f90f11df464bd7df0d6f47e0911aacfb30
unknown <longvuong@live.com>
2025-06-22 00:47:38 -0400

    refactored code

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,32 +1,35 @@
-import Task from "./Task"
-import NewTask from "./NewTask"
-import { Box, Divider } from "@mui/material"
-import type { TaskBoardProps } from "../types"
+import Task from "./Task";
+import NewTask from "./NewTask";
+import { Box, Divider } from "@mui/material";
+import type { TaskBoardProps } from "../types";
 
-export default function TaskBoard({ tasks, addTask, deleteTask, updateTask, boardType }: TaskBoardProps) {
-    return (
-        <>
-            <Box className="flex flex-col">
-                {
-                    tasks.map((task) => (
-                        <Task
-                            key={task.id}
-                            task={task.task}
-                            taskId={task.id}
-                            status={task.status}
-                            deleteTask={deleteTask}
-                            updateTask={updateTask}
-                        />
-                    ))
-                }
-                {/* MUI style > Tailwind for Divider margin */}
-                <Divider variant="middle" sx={{ my: 4 }} />
+export default function TaskBoard({
+  tasks,
+  addTask,
+  deleteTask,
+  updateTaskValue,
+  updateTaskStatus,
+  boardType,
+}: TaskBoardProps) {
+  return (
+    <>
+      <Box className="flex flex-col">
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task.task}
+            taskId={task.id}
+            status={task.status}
+            deleteTask={deleteTask}
+            updateTaskValue={updateTaskValue}
+            updateTaskStatus={updateTaskStatus}
+          />
+        ))}
+        {/* MUI style > Tailwind for Divider margin */}
+        <Divider variant="middle" sx={{ my: 4 }} />
 
-                <NewTask
-                    addTask={addTask}
-                    boardType={boardType}>
-                </NewTask>
-            </Box>
-        </>
-    )
+        <NewTask addTask={addTask} boardType={boardType}></NewTask>
+      </Box>
+    </>
+  );
 }
