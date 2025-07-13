d467cacae2f7193a6590fda277a8ebd444a061a9
staedtlernorica <longvuong@live.com>
2025-07-13 17:22:26 -0400

    center new task textfield

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,5 +1,4 @@
 import { useState, useRef } from "react";
-// import { useEffect } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import type { NewTaskProps, InputChangeEvent } from "../types";
 import AddTaskIcon from "@mui/icons-material/AddTask";
@@ -28,22 +27,24 @@ export default function NewTask({ addTask, boardType }: NewTaskProps) {
 
   return (
     <Box className="pt-3 w-full bg-gray-100 flex justify-center items-center">
-      <TextField
-        className="p0 m0 w-[45%]"
-        variant="standard"
-        placeholder="Add new task"
-        value={newTask}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-      ></TextField>
+      <div className="relative">
+        <TextField
+          className="w-[45vw]"
+          variant="standard"
+          placeholder="Add task"
+          value={newTask}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
 
-      <Button
-        className="ml-5 h-10 w-10 min-w-0 rounded-full"
-        variant="contained"
-        onClick={handleAddTask}
-      >
-        <AddTaskIcon />
-      </Button>
+        <Button
+          className="absolute left-full ml-[15px] h-10 w-10 min-w-0 rounded-full"
+          variant="contained"
+          onClick={handleAddTask}
+        >
+          <AddTaskIcon />
+        </Button>
+      </div>
     </Box>
   );
 }
